a39398573d9d666803f878f4e87b9026e8236324
Moises Beltran <BMoises@gmail.com>
2011-01-09 14:58:58 -0800

    Added lein project

---
+++ src/clojure_euler/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-euler.core)
---
+++ test/clojure_euler/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-euler.test.core
+  (:use [clojure-euler.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
