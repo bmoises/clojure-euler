1c1cf4bc4c17f268ca786472912d0f71d814a497
Moises Beltran <BMoises@gmail.com>
2011-05-19 07:53:09 -0700

    Updated documentation and refactored to use sqrt for factorization

--- src/problems/problem3.clj
+++ src/problems/problem3.clj
@@ -1,19 +1,24 @@
 (ns problems.problem3)
 
 (defn divisors
-  "Returns divisors for a given number"
-  [num, divisor, list]
+  "Returns divisors for a given number
+  Arguments:
+    num - Number you want to find factors for
+    num_sqrt - Square root of num
+    divisor - starting divisor
+    list - holds results"
+  [num, num_sqrt, divisor, list]
   ; (println (str num " -- " divisor))
   (if
-    (= num divisor) ; This is our stop condition
+    (> divisor num_sqrt) ; This is our stop condition
     (conj list num)
     (if 
       (zero? (mod num divisor)) ; If it divides the number add it to the list
-      (recur (/ num divisor) 2 (conj list divisor))
-      (recur num (+ divisor 1) list))))
+      (recur (/ num divisor) (Math/sqrt (/ num divisor)) 2 (conj list divisor))
+      (recur num num_sqrt (+ divisor 1) list))))
 
 (defn solution
 	"The prime factors of 13195 are 5, 7, 13 and 29.
 	 What is the largest prime factor of the number 600851475143 ?"
 	[n]
-	(sort (divisors n 2 [])))
+	(sort (divisors n (Math/sqrt n) 2 [])))
