85688f816f3e689583cb3214a70ec437e1a47ead
Moises Beltran <BMoises@gmail.com>
2011-05-18 22:53:00 -0700

    Optimized recursive function to use 'recur' intead of recursively calling divisor itself. That way this does not blow up the stack

--- src/problems/problem3.clj
+++ src/problems/problem3.clj
@@ -3,18 +3,17 @@
 (defn divisors
   "Returns divisors for a given number"
   [num, divisor, list]
-  (println (str num " -- " divisor))
+  ; (println (str num " -- " divisor))
   (if
-    (= num divisor) 
+    (= num divisor) ; This is our stop condition
     (conj list num)
     (if 
-      (zero? (mod num divisor))
-      (conj (divisors (/ num divisor) 2 list) divisor)
-      (divisors num (+ divisor 1) list))))
+      (zero? (mod num divisor)) ; If it divides the number add it to the list
+      (recur (/ num divisor) 2 (conj list divisor))
+      (recur num (+ divisor 1) list))))
 
 (defn solution
 	"The prime factors of 13195 are 5, 7, 13 and 29.
 	 What is the largest prime factor of the number 600851475143 ?"
 	[n]
-	(sort (divisors n 2 []))
-	)
+	(sort (divisors n 2 [])))
