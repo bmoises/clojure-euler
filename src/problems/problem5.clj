ade419e0e06c6a334ab3faff5635f5948a1d3c6c
Moises Beltran <BMoises@gmail.com>
2011-06-01 22:22:01 -0700

    Added some comments

--- src/problems/problem5.clj
+++ src/problems/problem5.clj
@@ -4,6 +4,9 @@
   "Recursively call itself to find the solution"
   [my_range num]
     ; (println (str "Processing: " num))
+    ; We check to see if the sum of the following is > 0, if so, 
+    ; we inc num and do it all over again
+    ; If not, we stop
     (if (> (apply + (map #(mod num %) my_range)) 0)
       (recur my_range (inc num))
       num))
@@ -15,7 +18,7 @@
    What is the smallest positive number that is evenly 
    divisible by all of the numbers from 1 to 20?"
 	[start end]
-	
+	; Define my_range only once
 	(let [my_range (range start (+ end 1))]
 	
 	  (find_all my_range 1)
